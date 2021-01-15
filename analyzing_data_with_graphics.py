be3f8ab52cc57dd208a6a8b9e1688a33927fd4e9
rodrigo-meyer <76924861+rodrigo-meyer@users.noreply.github.com>
2021-01-15 16:11:08 -0300

    Add files via upload

---
+++ analyzing_data_with_graphics.py
@@ -0,0 +1,68 @@
+# Analyzing Data with graphics.
+
+# Program Variables.
+prog_name = 'Life Goals.'
+visual = ' '
+# visual = '█'
+# visual = '*'
+
+g_size_goal = 0
+g_size_actual = 0
+
+print(f'\n\033[0;30;44m {prog_name:^52} \033[m\n')
+
+while True:
+
+    goal_name = str(input(f'Goal name: '))
+    goal_value = int(input(f'Goal {goal_name}: '))
+    actual_value = int(input(f'Actual {goal_name}: '))
+    print('_' * 52)
+
+    if goal_value > 9999 or actual_value > 9999:
+
+        goal_value = int(goal_value)
+        g_size_goal = int(goal_value / 1000000)
+
+        actual_value = int(actual_value)
+        g_size_actual = int(actual_value / 1000000)
+
+    elif goal_value > 999 or actual_value > 999:
+
+        goal_value = int(goal_value)
+        g_size_goal = int(goal_value / 1000)
+
+        actual_value = int(actual_value)
+        g_size_actual = int(actual_value / 1000)
+
+    elif goal_value > 99 or actual_value > 99:
+
+        goal_value = int(goal_value)
+        g_size_goal = int(goal_value / 100)
+
+        actual_value = int(actual_value)
+        g_size_actual = int(actual_value / 100)
+
+    elif goal_value > 9 or actual_value > 9:
+
+        goal_value = int(goal_value)
+        g_size_goal = int(goal_value)
+
+        actual_value = int(actual_value)
+        g_size_actual = int(actual_value)
+
+    print(f'  Goal {goal_name:<10} {goal_value:>10} \033[0;30;42m {visual * g_size_goal} \033[m\n')
+    print(f'Actual {goal_name:<10} {actual_value:>10} \033[0;30;41m {visual * g_size_actual} \033[m')
+    print('_' * 50)
+    option = str(input('\033[0;30;44m Enter \033[m to add another or type \033[0;30;44m xxx \033[m to finish.')).upper()
+    print('_' * 52)
+
+    goal_value = 0
+    actual_value = 0
+    g_size_goal = 0
+    g_size_actual = 0
+
+    if option == ('XXX'):
+
+        break
+
+print(f'\n\033[0;30;45m {"":^44} \033[m\n\n')
