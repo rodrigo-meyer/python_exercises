8b7dab392718962888f7b0c9db61c2329756379e
rodrigo-meyer <76924861+rodrigo-meyer@users.noreply.github.com>
2021-01-18 09:26:23 -0300

    Add files via upload

---
+++ opening_the_browser.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+search_original = str(input("\nSearch in "
+                            "\033[1;34mG\033[m"
+                            "\033[1;31mo\033[m"
+                            "\033[1;33mo\033[m"
+                            "\033[1;34mg\033[m"
+                            "\033[1;32ml\033[m"
+                            "\033[1;31me\033[m: "))
+
+search_final = search_original.replace(" ", "+")
+
+webbrowser.open(f'https://www.google.com/search?source=hp&ei=YnMFYMXUBN-65OUPsMG7-A8&iflsig=AINFCbYAAAAAYAWBcgw480xl4AqRRlfEZHsOeFPd8nH-&q={search_final}')
